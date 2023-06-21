eccf379e3c6ef6ea2cc88d886d891e693d54cb1c
Zeynep Deniz <zeynep351961@gmail.com>
2023-06-22 02:19:42 +0300

    add mongodb

--- backend/index.js
+++ backend/index.js
@@ -30,3 +30,16 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+
+app.post("/auth/register", upload.single("picture"), register);
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
